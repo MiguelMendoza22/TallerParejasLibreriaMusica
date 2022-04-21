c831e622e3e4db7f76a036b13eca3fef74b9bb00
Droco20 <99331695+Droco20@users.noreply.github.com>
2022-04-21 16:24:15 -0500

    Update Library.java
    Agrego atributos y métodos de la clase Library

--- TallerParejas/src/com/sofka/Library.java
+++ TallerParejas/src/com/sofka/Library.java
@@ -1,4 +1,54 @@
 package com.sofka;
 
-public class Library {
+import java.util.ArrayList;
+
+public abstract class Library {
+     public ArrayList<Song> songs;
+
+     /**
+      * Metodo Constructor que permite crear una instancia de la clase Library.
+      * 
+      */
+     public Library(ArrayList<Song> songs) {
+          this.songs = songs;
+     }
+
+     /**
+      * Metodo de acceso pare el atributo songs.
+      * 
+      */
+     public ArrayList<Song> getSongs() {
+          return songs;
+     }
+
+     /**
+      * Metodo modificador del atributo songs.
+      * 
+      */
+     public void setSongs(ArrayList<Song> songs) {
+          this.songs = songs;
+     }
+
+     /**
+      * este metodo permite agregar una canción al array.
+      *
+      */
+     public void addSong(Song s) {
+          this.songs.add(s);
+     }
+
+     /**
+      * este metodo permite acceder a una canción del array.
+      * 
+      */
+     public Song getSong(int id) {
+          int index = 0;
+          for (int i = 0; i < songs.size(); i++) {
+               if (songs.get(i).getId() == id) {
+                    index = i;
+               }
+          }
+          return this.songs.get(index);
+     }
 }
+
