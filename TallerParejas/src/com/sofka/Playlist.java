90098fa66bb3b19aa0819a40be644dee43ca6a47
Droco20 <99331695+Droco20@users.noreply.github.com>
2022-04-24 10:38:06 -0500

    clase Playlist
    Modifico la clase Playlist  agregando el atributo y métodos constructor y de acceso . cuya finalidad permite crear nuevas listas de canciones.

--- TallerParejas/src/com/sofka/Playlist.java
+++ TallerParejas/src/com/sofka/Playlist.java
@@ -1,4 +1,34 @@
 package com.sofka;
 
-public class Playlist {
+
+import java.util.ArrayList;
+
+public class Playlist extends Library {
+
+     private String namePlist;
+
+     /** Metodo que permite crear una instancia de la clase Playlist. */
+
+     public Playlist(String namePlist, ArrayList<Song> songs) {
+          super(songs);
+          this.namePlist = namePlist;
+     }
+
+     /**Metodos de acceso para el atributo namePlist  */
+
+     public String getNamePlist() {
+          return namePlist;
+     }
+
+     public void setNamePlist(String namePlist) {
+          this.namePlist = namePlist;
+     }
+
+     @Override
+     public String toString() {
+         return "Playlist: " + namePlist + "\n" + getSongs();
+     }
+
+     
+
 }
